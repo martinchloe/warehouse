22495a1395689496f98ed28d96cf8a2112a54ee0
martinchloe <chloe.martin0@gmail.com>
2020-07-23 15:57:24 +0200

    23/07/20 16h

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,43 @@
 var mySwiper = new Swiper('.swiper-container', {
   loop: true,
-    // autoplay: true,
+    autoplay: true,
   pagination: {
       el: '.swiper-pagination',
       clickable: true,
   },
 })
 
+const header = document.querySelector('header');
+const links = header.querySelectorAll('a');
 const burger = document.querySelector('.burger');
 const toggleMenu = document.querySelector('.toggle-menu');
 const closeButton = document.querySelector('.close-button');
+console.log(links)
 
 function displayMenu(){
-  toggleMenu.style.right = 0;
+  toggleMenu.style.left = '10vh';
+  toggleMenu.style.right = '0';
 }
 
 function closeMenu(){
-  toggleMenu.style.right = '100%';
+  toggleMenu.style.left = '90vw';
 }
 
 burger.addEventListener('click', displayMenu);
 closeButton.addEventListener('click', closeMenu);
+
+
+window.addEventListener('scroll', function(){
+  if(window.scrollY > 7){
+    header.style.background = "#fff";
+    header.style.padding = "1rem 0";
+    header.style.boxShadow = "4px 0 20px -5px rgba(0, 0, 0, 0.2)";
+    links.forEach(link => link.style.color = "#000");
+  } 
+  if(window.scrollY == 0){
+    header.style.background = "transparent";
+    header.style.color = "#fff";
+    header.style.padding = "1.5rem 0";
+    links.forEach(link => link.style.color = "rgba(255, 255, 255, 0.5)");
+  }
+})
