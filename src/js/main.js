332ffdc5bef41131f471d041b239b533a948e519
martinchloe <chloe.martin0@gmail.com>
2020-07-23 11:59:46 +0200

    23/07/20 12h

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,23 @@
 var mySwiper = new Swiper('.swiper-container', {
-    loop: true,
+  loop: true,
     // autoplay: true,
-    pagination: {
+  pagination: {
       el: '.swiper-pagination',
       clickable: true,
-    },
-  })
+  },
+})
+
+const burger = document.querySelector('.burger');
+const toggleMenu = document.querySelector('.toggle-menu');
+const closeButton = document.querySelector('.close-button');
+
+function displayMenu(){
+  toggleMenu.style.right = 0;
+}
+
+function closeMenu(){
+  toggleMenu.style.right = '100%';
+}
+
+burger.addEventListener('click', displayMenu);
+closeButton.addEventListener('click', closeMenu);
